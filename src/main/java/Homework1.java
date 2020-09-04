a7443eec07c60b49606ec3d2fe3df035eb347134
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-09-04 19:49:45 +0300

    1 done

---
+++ src/main/java/Homework1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double a = input.nextDouble();
+        System.out.print("Enter the second number: ");
+        double b = input.nextDouble();
+        double value = b/a;
+        String Result = String.format("%.4f",value);
+        System.out.print("Addition: " + Result);
+    }
+}
