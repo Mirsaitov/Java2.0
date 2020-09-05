7298d50d7d5015e820612f8edef3e577ed29ca3e
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-09-05 08:12:38 +0300

    2 done

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class Homework2 {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        double a = input.nextDouble();
-        System.out.print("Enter the second number: ");
-        double b = input.nextDouble();
-        double value = b / a;
-        String Result = String.format("%.4f", value);
-        System.out.print("Addition: " + Result);
+        System.out.print("Введите первую цифру: ");
+        double a = input.nextDouble();               //Вводим первую цифру
+        System.out.print("Введите тип операции (+,-,/,*): ");
+        String x = input.next();                     //Выбираем тип операции
+        System.out.print("Введите вторую цифру: ");
+        double b = input.nextDouble();               //Вводим вторую цифру
+
+        double value = 0;
+
+        if (x.equals("/")) value = a / b;           // С помощью оператора if else определяем какую команду выполнить
+        else if (x.equals("*")) value = a * b;
+        else if (x.equals("+")) value = a + b;
+        else if (x.equals("-")) value = a - b;
+        else System.out.print("Неверный формат ввода" + "\n");
+
+        System.out.print("Результат: " + String.format("%.4f", value));   // Выводим на экран
+
     }
 }
