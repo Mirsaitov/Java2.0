3a575ea85d59f8d1d147fa271d6b5529459cecb4
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-09-07 09:09:57 +0300

    3 done

--- src/main/java/Homework3.java
+++ src/main/java/Homework3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Homework3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите цифру: ");
+        /*System.out.print("Введите цифру: ");
         int y = input.nextInt();
         if (y == 1) {
             System.out.print("Введите первую цифру: ");
@@ -24,6 +24,24 @@ public class Homework3 {
 
             System.out.print("Результат: " + String.format("%.4f", value));   // Выводим на экран
         }
-        else if (y==2) {}
-    }
+        else if (y==2) {} */
+
+
+        System.out.print("Шаг 1: " + "\n" + "Введите размер массива: ");
+        int size = input.nextInt();
+        String nums[] = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Введите данные массива" + " №" + (i+1) + ": ");
+            nums[i] = input.next();                         // Заполняем массив элементами, введёнными с клавиатуры
+
+
+            }
+        }
+
+
+
+
+
+        }
 }
