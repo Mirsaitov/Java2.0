1ed570a30e9129b9bda2870534de9d9bce0ae952
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-09-27 23:11:54 +0300

    H7 done

---
+++ src/main/java/Homework7/Operand.java
@@ -0,0 +1,17 @@
+package Homework7;
+
+/**
+ *  Выражения для калькулятора
+ */
+
+public class Operand {
+    private double value;
+
+    Operand(double value) {
+        this.value = value;
+    }
+
+    double getValue() {
+        return value;
+    }
+}
